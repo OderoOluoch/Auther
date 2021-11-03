e66b272242a77714ed307a10f01454c9261e6bb7
OderoOluoch <oderoluoch@gmail.com>
2021-11-04 00:00:26 +0300

    User authentication and authorization

---
+++ src/test/java/io/odero/auther/AutherApplicationTests.java
@@ -0,0 +1,13 @@
+package io.odero.auther;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
