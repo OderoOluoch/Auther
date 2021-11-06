3fdcb30b99ecfc60589d61ea69e6bc1c9711e880
OderoOluoch <oderoluoch@gmail.com>
2021-11-07 01:07:40 +0300

    We have enabled form based authentication

--- src/main/java/io/odero/auther/security/ApplicationSecurityConfig.java
+++ src/main/java/io/odero/auther/security/ApplicationSecurityConfig.java
@@ -45,7 +45,7 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest()
                 .authenticated()
                 .and()
-                .httpBasic();
+                .formLogin();
     }
 
     @Override
